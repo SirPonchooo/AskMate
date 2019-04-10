853bf1d1988a8490dfe6d661c2846fa1d7ad6a69
Axwoksal <ikalas.monikalaskowska@gmail.com>
2019-04-10 11:18:58 +0200

    add a question

--- server.py
+++ server.py
@@ -7,5 +7,11 @@ app = Flask(__name__)
 def main_page():
     return render_template('index.html')
 
+@app.route('/add-question', methods=['GET', 'POST'])
+def display_add_question():
+    return render_template('add-question.html')
+
+
+
 if __name__ == '__main__':
     server.run()
