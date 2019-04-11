8d8bf73005e294af64b82733ff56bad08efc32e4
Axwoksal <ikalas.monikalaskowska@gmail.com>
2019-04-11 14:31:33 +0200

    add a question

--- connection.py
+++ connection.py
@@ -1,7 +1,7 @@
 import csv
 import os
 
-def import_data(filename):  # import csv data
+def import_data(filename): # import csv data
     with open(filename, "r") as file:
         imported_data = csv.DictReader(file)
         data = list(imported_data)
@@ -13,7 +13,7 @@ def write_file(dict_list, filename):
     keys = ["id", "submission_time", "view_number", "vote_number", "title", "message", "image"]
     if filename == 'answers.csv':
         keys = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
-    file_directory = 'AskMate/sample_data/' + filename
-    with open(file_directory, 'a+', newline='') as f:
+    #file_directory = 'AskMate/sample_data/' + filename
+    with open(filename, 'a', newline='') as f:
         writer = csv.DictWriter(f, keys)
         writer.writerow(dict_list)
