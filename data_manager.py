04abcc5c4070057c0f1ce95f2e99f764f0780935
SirPonchooo <poncho.sobczyski@gmail.com>
2019-04-11 14:34:52 +0200

    nic

--- data_manager.py
+++ data_manager.py
@@ -1,8 +1,11 @@
 import connection
 
+
 def get_all_answers():
     a_list = connection.import_data('answers.csv')
     return a_list
 
+
 def get_questions():
     questions = connection.import_data('question.csv')
+    return questions
--- server.py
+++ server.py
@@ -1,17 +1,27 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, redirect, request
+
+import data_manager
 
 app = Flask(__name__)
 
 
+list_of_roccs = ["big rock", "shmaller roczkk"]
+
 @app.route('/')
 def main_page():
     return render_template('index.html')
+
 
 @app.route('/add-question', methods=['GET', 'POST'])
 def display_add_question():
     return render_template('add-question.html')
 
 
+@app.route('/list', methods=['GET', 'POST'])
+def display_list_of_questions():
+    list_of_questions = data_manager.get_questions()
+    return render_template('list.html', questions=list_of_questions)
+
 
 if __name__ == '__main__':
-    server.run()
+    app.run(debug=True)
